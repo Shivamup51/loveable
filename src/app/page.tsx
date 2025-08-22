62dd2339416090247c5b8a47beec33cc0f542257
Shivamup51 <shivamupadhyay2868@gmail.com>
2025-08-22 22:44:22 +0530

    Add Prisma integration and update dependencies
    - Added Prisma schema file for database models (User and Post).
    - Introduced Prisma client in the database utility file.
    - Updated package.json and package-lock.json to include Prisma and related dependencies.
    - Modified .gitignore to exclude generated Prisma files.
    - Cleaned up the main page component formatting.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
-import Image from "next/image";
 
-export default function Home() {
+
+export default  function Home() {
+
   return (
     <div className="">
-      hello world
+     
     </div>
   );
 }
