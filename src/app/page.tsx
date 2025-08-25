cc60ec8ed0d5b8bd0fb648209cd0c23e602ad49e
Shivamup51 <shivamupadhyay2868@gmail.com>
2025-08-26 00:00:37 +0530

    Update Inngest integration and UI enhancements
    - Added @inngest/agent-kit as a dependency for background job processing.
    - Updated the layout to include a Toaster component for notifications.
    - Enhanced the main page to accept user input and trigger background jobs with success notifications.
    - Modified the TRPC router to handle input changes from 'text' to 'value' for better clarity.
    - Implemented a new getResults query placeholder for future result handling.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,16 +3,26 @@
 import { Button } from "@/components/ui/button";
 import { useTRPC } from "@/trpc/client";
 import { useMutation } from "@tanstack/react-query";
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
+import { toast } from "sonner";
 
 
-const page = async () => {
+const page = () => {
+
+  const [value, setValue] = useState<string>("");
   const trpc = useTRPC();
 
-  const invoke = useMutation(trpc.invoke.mutationOptions({}))
+  const invoke = useMutation(trpc.invoke.mutationOptions({
+    onSuccess:()=>{
+      toast.success("Background job invoked");
+    }
+  }));
 
   return (
    <div className="p-4 max-w-7xl mx-auto">
-    <Button onClick={()=>invoke.mutate({text:"hello"})}>Invoke background job</Button>
+    <Input value={value} onChange={(e)=>setValue(e.target.value)} />
+    <Button disabled={invoke.isPending} onClick={()=>invoke.mutate({value:value})}>Invoke background job</Button>
    </div>
   )
 }
