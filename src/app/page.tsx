255cda24e35012780e1f5833754b1a2a6bca5f4d
Shivamup51 <shivamupadhyay2868@gmail.com>
2025-08-27 19:49:12 +0530

    Add project management features and integrate project ID in messages
    - Introduced Project model and related changes in Prisma schema.
    - Updated Message model to include projectId for association.
    - Implemented project creation with message handling in procedures.
    - Enhanced page components to display project details.
    - Added random-word-slugs package for project name generation.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,32 +2,35 @@
 
 import { Button } from "@/components/ui/button";
 import { useTRPC } from "@/trpc/client";
-import { useMutation, useQuery,  } from "@tanstack/react-query";
+import { useMutation } from "@tanstack/react-query";
 import { Input } from "@/components/ui/input";
 import { useState } from "react";
 import { toast } from "sonner";
+import { useRouter } from "next/navigation";
 
 
 const page = () => {
-
+  const router  = useRouter();
   const [value, setValue] = useState<string>("");
   const trpc = useTRPC();
 
-  const {data:messages} = useQuery(trpc.messages.getMany.queryOptions());
-
-  const createMessage = useMutation(trpc.messages.create.mutationOptions({
-    onSuccess:()=>{
-      toast.success("message created");
+  const createProject = useMutation(trpc.projects.create.mutationOptions({
+    onError: (error) => {
+      toast.error(error.message);
+    },
+    onSuccess: (data) => {
+      router.push(`/projects/${data.id}`);
     }
   }));
 
-  return (
-   <div className="p-4 max-w-7xl mx-auto">
-    <Input value={value} onChange={(e)=>setValue(e.target.value)} />
-    <Button disabled={createMessage.isPending} onClick={()=>createMessage.mutate({value:value})}>Invoke background job</Button>
-    {JSON.stringify(messages , null , 2)}
-   </div>
 
+  return (
+    <div className="h-screen w-screen flex items-center justify-center ">
+      <div className="max-w-7xl mx-auto flex items-center flex-col gap-y-4 justify-center ">
+        <Input value={value} onChange={(e) => setValue(e.target.value)} />
+        <Button disabled={createProject.isPending} onClick={() => createProject.mutate({ value: value })}>Submit</Button>
+      </div>
+    </div>
   )
 }
 
